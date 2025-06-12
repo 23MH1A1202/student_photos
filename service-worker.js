7acf7788a2945baf50752647d1c89cfb9519d16a
Lalitha Sagar Ambati <23mh1a1202@acoe.edu.in>
2025-06-12 16:02:08 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,24 @@
+const CACHE_NAME = 'offline-cache-v1';
+const OFFLINE_URL = 'offline.html';
+
 self.addEventListener('install', (event) => {
   console.log('Service Worker installed.');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/offline.html'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request).then((cachedResponse) => {
+        return cachedResponse || caches.match(OFFLINE_URL);
+      });
+    })
+  );
 });
